4134e2b606f610f47744aff005c499b21319cab1
Ejaz Merchant <ejaz.merchant@icloud.com>
2018-03-01 01:10:43 -0500

    Converted to MVC design pattern

---
+++ ToDoList/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by Ejaz Merchant on 3/1/18.
+//  Copyright © 2018 Ejaz Merchant. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
